0b4d00f478251f79119c18f4904aa497cb1946f8
rlllup0084 <llup.renee@gmail.com>
2025-01-10 13:12:17 +0800

    Re-add Account then add more properties

--- src/features/account/types.ts
+++ src/features/account/types.ts
@@ -1,3 +1,5 @@
+import { Models } from "node-appwrite";
+
 export const FleetSizeEnum = {
     LESS_THAN_5: "<5",
     BETWEEN_6_10: "6-10",
@@ -42,3 +44,17 @@ export const CompanyRoleEnum = {
     EMPLOYEE: "employee",
     OTHER: "other"
 } as const;
+
+export type Account = Models.Document & {
+    company_name: string,
+    fleet_size: "<5" | "6-10" | "11-20" | "21-50" | "51-100" | "101-500" | "501-1000" | ">1000",
+    industry:  "technology" | "healthcare" | "finance" | "manufactuting" | "retail" | "education" | "transportation" | "construction" | "agriculture" | "real_estate" | "energy" | "telecom" | "media" | "entertainment" | "government" | "hospitality" | "non_profit" | "aerospace" | "automotive" | "logistics" | "pharmaceutical" | "insurance" | "sports" | "other",
+    company_role:  "owner" | "manager" | "employee" | "other",
+    goals:  string,
+    enable_demo_data: boolean,
+    steps_done: number,
+    password: string;
+    traccar_api: string;
+    username: string;
+    vin_decoder_key: string;
+};
